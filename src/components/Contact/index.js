5af0b9bb69a249ff6b5a053aec7ff2727e85d3d8
Mark Granade <88593585+MarkGranade@users.noreply.github.com>
2022-01-16 15:22:29 -0500

    added handleSubmit and handleChange functions to Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactForm = () => {
+	// Hook that'll manage the form data.
+	const [formState, setFormState] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+	const { name, email, message } = formState;
+
+	// Sync the internal state of the componenet "formState" with the user input from the DOM.
+	// The "onChange" event listener will ensure that the "handleChange" function fires whenever a keystroke is typed into the input field for "name".
+	const handleChange = (e) => {
+		// Using the "setFormState" function to update the "formState" value for the "name" property.
+		// We use the spread operator, "...formState", so we can retain the other key-value pairs in this object. Without the spread operator, the "formState" object would be overwritten to only contain the " name: value" key pair.
+		setFormState({ ...formState, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formState);
+	};
+
+	// console.log(formState);
+
 	return (
 		<section>
 			<h1>Contact me</h1>
-			<form id="contact-form">
+			<form id="contact-form" onSubmit={handleSubmit}>
 				<div>
 					{/* Due to keywords reserved in JavaScript, we need to replace the "for" attribute in the <label> element to "htmlFor" */}
 					<label htmlFor="name">Name:</label>
-					<input type="text" name="name" />
+					<input
+						type="text"
+						name="name"
+						defaultValue={name}
+						onChange={handleChange}
+					/>
 				</div>
+
 				<div>
 					<label htmlFor="email">Email address:</label>
-					<input type="email" name="email" />
+					<input
+						type="email"
+						name="email"
+						defaultValue={email}
+						onChange={handleChange}
+					/>
 				</div>
+
 				<div>
 					<label htmlFor="message">Message:</label>
-					<textarea name="message" rows="5" />
+					<textarea
+						name="message"
+						rows="5"
+						defaultValue={message}
+						onChange={handleChange}
+					/>
 				</div>
+
 				<button type="submit">Submit</button>
 			</form>
 		</section>
